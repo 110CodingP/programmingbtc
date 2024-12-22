8c183b1883622335869757527f6a4a10b241dd7e
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2024-12-22 21:24:20 +0100

    implemented tests for signature verifications

--- src/ec_cryptography/src/lib.rs
+++ src/ec_cryptography/src/lib.rs
@@ -366,8 +366,8 @@ mod tests {
         complete();     // The order of the secp256k1 curve. At multiple of this value, the curve become Infinity.
         let generator_point = secp_generator_point();
 
-        let point_x = Integer::parse_radix("04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574", 16).unwrap().complete();
-        let point_y = Integer::from_str_radix("82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4", 16).unwrap();
+        let point_x = Integer::parse_radix("887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c", 16).unwrap().complete();
+        let point_y = Integer::from_str_radix("61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34", 16).unwrap();
 
         let point = EllipticCurve::secp_point(
             point_x, 
@@ -375,18 +375,18 @@ mod tests {
         );
 
         let z = FieldElement::new(
-            Integer::from_str_radix("bc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423", 16).unwrap(),
+            Integer::from_str_radix("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", 16).unwrap(),
             order.clone()
         );
         let r = FieldElement::new(
             Integer::parse_radix(
-                "37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6", 
+                "eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c", 
                 16)
                 .unwrap()
                 .complete(),
             order.clone()
         );
-        let s = FieldElement::new(Integer::parse_radix("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec", 16).unwrap().complete(), order.clone());
+        let s = FieldElement::new(Integer::parse_radix("c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6", 16).unwrap().complete(), order.clone());
 
         let u = z / s.clone();
         let v = r.clone() / s;
